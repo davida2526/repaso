6ab5705fd3cf885c8075a9c112a8b6597672fd6d
davida2526 <101894119+davida2526@users.noreply.github.com>
2022-03-24 17:29:06 -0500

    david santiago quiroga 1102

---
+++ actividad.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<body>
+    <h1>hola soy un titulo</h1>
+    <?php
+    //inicia codigo php
+    //esto es un comentario
+    //comando echo devuelve un valor
+    echo "hola soy un parrafo";
+    echo "<br>"; //salto de linea
+    echo 3+3; 
+    ?>
+</body>
+</html>
