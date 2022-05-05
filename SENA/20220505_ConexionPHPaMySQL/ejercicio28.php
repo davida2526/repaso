9bd9b2152caf2d7ae584b86d4fb4560d3cc9521e
davida2526 <101894119+davida2526@users.noreply.github.com>
2022-05-05 17:02:22 -0500

    Update ejercicio28.php

--- SENA/20220505_ConexionPHPaMySQL/ejercicio28.php
+++ SENA/20220505_ConexionPHPaMySQL/ejercicio28.php
@@ -5,17 +5,11 @@ $usuario="root";
 $contrasena="";
 
 try{
-
-
 $conexion=new PDO("mysql:host=$servidor;dbname=album", $usuario,$contrasena );
 $conexion->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
 echo "conexion establecida";
 
 }catch(PDOException $error){
-    echo "conexion erronea".$error;
-      
+    echo "conexion erronea".$error;     
 }
-
-
-
 ?>
